fab4ff70b3e9352de34343b416cfffe49ea0c511
victorhelopes <victorhelopes@gmail.com>
2024-03-26 14:45:48 -0300

    update: mobile vision

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
   <main>
     <div class="input">
-      <input type="file" @change="handleFileUpload" ref="file"/>
+      <div>
+        <p>Importe o arquivo .csv para fazer o cálculo das metricas</p>
+        <input type="file" @change="handleFileUpload" ref="file"/>
+      </div>
       <button @click="getMetrics">Carregar metricas</button>
     </div>
-    <div class="charts">
+    <div v-if="showError">
+      <p>input a file</p>
+    </div>
+    <div v-if="chartChurnData.labels.length > 0 || chartMRRData.labels.length > 0" class="charts">
       <div>
         <h3>Churn</h3>
         <Bar
@@ -47,6 +53,7 @@ export default{
   data() {
       return {
         file: '',
+        showError: false,
         chartChurnData: {
           labels: [] as string[],
           datasets: [ { data: [] as number[] } ]
@@ -57,7 +64,7 @@ export default{
         },
         chartOptions: {
           responsive: true
-        }
+        },
       }
     },
 
@@ -65,6 +72,12 @@ export default{
     async getMetrics(){
       let formData = new FormData();
       formData.append('file', this.file);
+      if(!this.file || this.file === '' ){
+        this.showError = true;
+        return;
+      }else{
+        this.showError = false
+      }
       const mrr: number[] = []
       const churn: number[] = []
       this.chartChurnData.labels = []
@@ -81,7 +94,11 @@ export default{
             mrr.push(mrrInfo.data)
           }
         })
-      })
+      }).catch(()=>{
+          this.showError = true;
+        return;
+        }
+      )
         this.chartChurnData.datasets[0].data = churn
         this.chartMRRData.datasets[0].data = mrr
     },
@@ -96,29 +113,42 @@ export default{
 <style scoped>
   .input{
     display: flex;
-    align-items: center;
-    button{
-      margin-left: 80px;
-      background-color: rgb(129, 131, 224);
-      border: none;
-      border-radius: 8px;
-      padding: 10px;
-      color: white;
-      cursor: pointer;
-    }
+    align-items: flex-start;
+    flex-direction: column;
+  }
+  button{
+    background-color: rgb(129, 131, 224);
+    border: none;
+    border-radius: 8px;
+    padding: 10px;
+    color: white;
+    cursor: pointer;
+    margin-top: 16px;
   }
 
   .charts{
     display: flex;
     align-items: center;
     justify-content: space-between;
+    flex-wrap: wrap;
     margin-top: 10px;
     div{
       width: 100%;
+      max-width: 500px;
       canvas{
         width: 100%;
-        max-width: 500px;
       }
     }
   }
+
+@media (min-width: 1024px){
+  .input{
+    flex-direction: row;
+    align-items: center;
+  }
+
+  button{
+    margin-left: 80px;
+  }
+}
 </style>
